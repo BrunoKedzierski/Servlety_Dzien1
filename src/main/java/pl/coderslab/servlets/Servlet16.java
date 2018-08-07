4bb6a1751b460c65a0ed0d0545f1818afbcae9ac
bruno <bruno.kedzierski@wp.pl>
2018-08-07 22:14:16 +0200

    Zadania

---
+++ src/main/java/pl/coderslab/servlets/Servlet16.java
@@ -0,0 +1,36 @@
+package pl.coderslab.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(name = "Servlet16", urlPatterns = "/servlet16")
+public class Servlet16 extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Map<String, String> map = new HashMap<String, String>();
+        StringBuilder builder = new StringBuilder();
+        Enumeration headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String key = (String) headerNames.nextElement();
+            String value = request.getHeader(key);
+            map.put(key, value);
+        }
+
+        for (Map.Entry el : map.entrySet()) {
+            builder.append(el.getKey() + " : " + el.getValue() + "\n");
+        }
+        response.getWriter().append(builder);
+    }
+
+}
+
